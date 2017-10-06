112ba8ed395ffa1a454ff1af8c6328d321bf4e0d
Gary Howard <garyhowardpctech@yahoo.com>
2017-10-05 20:11:09 -0400

    Changed some methods

--- src/TaskCollection.php
+++ src/TaskCollection.php
@@ -48,9 +48,6 @@ class TaskCollection implements \Countable
     public function addTask(TaskInterface $task)
     {
         $this->dbInsert($task);
-        $task->setId($this->getIdFromDb());
-        $this->tasks[] = $task;
-
         return $this;
     }
 
@@ -59,26 +56,49 @@ class TaskCollection implements \Countable
      *
      * @return string
      */
-    private function getIdFromDb()
+    public function getIdFromDb()
     {
         return $this->pdo->lastInsertId();
     }
 
-    /**
-     * Removes a task from the collection.
-     *
-     * @param integer $id
-     * @return \GMH\Task | null
-     */
     public function removeTask($id)
     {
-        if (in_array($id, array_keys($this->tasks))) {
-            $removedTask = $this->tasks[$id];
-            unset($this->tasks[$id]);
+        return $this->removeTaskFromDb($id);
+    }
 
-            return $removedTask;
+    /**
+     * Removes a task from the database.
+     *
+     * @param mixed $id
+     * @return void
+     */
+    private function removeTaskFromDb($id)
+    {
+        $stmt1 = $this->pdo->prepare("SELECT * FROM tasks WHERE id=:id");
+        $stmt1->setFetchMode(\PDO::FETCH_CLASS, '\GMH\Task');
+        $stmt1->execute([':id' => $id]);
+        $removedTask = $stmt1->fetch();
+        
+        $stmt2 = $this->pdo->prepare("DELETE FROM tasks WHERE id=:id");
+        $stmt2->execute([':id' => $id]);
+
+        return $removedTask;
+    }
+
+    /**
+     * 
+     */
+    public function getAllTasks()
+    {
+        $sql = 'SELECT * FROM tasks';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->setFetchMode(\PDO::FETCH_CLASS, '\GMH\Task');
+        $stmt->execute();
+
+        while ($task = $stmt->fetch()) {
+            $this->tasks[] = $task;
         }
-        return;
+        return $this->tasks;
     }
 
     /**
@@ -89,7 +109,9 @@ class TaskCollection implements \Countable
      */
     private function dbInsert(Task $task)
     {
-        $sql = "INSERT INTO tasks values(null, :name, :dueDate)";
+        if (is_null($task->getId())) {
+            $sql = "INSERT INTO tasks values(null, :name, :dueDate)";
+        }
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute([':name' => $task->getName(), ':dueDate' => $task->getDueDate()]);
     }
